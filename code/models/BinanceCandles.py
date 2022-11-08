786c1787542d39477278053a6001b43ec558d613
Richard Chamberlain <59230129+sbspros@users.noreply.github.com>
2022-11-07 20:33:13 -0600

    Add files via upload

--- code/models/BinanceCandles.py
+++ code/models/BinanceCandles.py
@@ -9,13 +9,25 @@ class CandleStickFailed(Exception):
         super().__init__(self.msg)  
         
 class BinanceCandles():
+
+    high=2
+    low=3
+    open=1
+    close=4
+
     def __init__(self,bc:BaseClass,conn:BinanceConnection):
         self._bc=bc
         self._conn=conn
 
     def candle_stick(self,symbol:str,interval:str):
         try:
-            return self._conn._client.get_klines(symbol=symbol, interval=interval):
+            self._candle_list = self._conn._client.get_klines(symbol=symbol, interval=interval)
         except:
             self._bc.log.error("\t"+":"+traceback.format_exc())
-            raise TickerFailed()  
+            raise CandleStickFailed() 
+
+    def get_candle(self,which_one):
+        return {'high':self._candle_list[which_one][BinanceCandles.high],\
+            'low':self._candle_list[which_one][BinanceCandles.low],\
+            'open':self._candle_list[which_one][BinanceCandles.open],\
+            'close':self._candle_list[which_one][BinanceCandles.close]}   
