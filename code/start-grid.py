f9519cb904cfaa2e86de3acb425221aef3656462
Richard Chamberlain <59230129+sbspros@users.noreply.github.com>
2022-11-09 05:15:40 -0600

    Add files via upload

--- code/start-grid.py
+++ code/start-grid.py
@@ -1,11 +1,14 @@
 from common.BaseClass import BaseClass,AppException
 from models.BinanceConnection import BinanceConnection,BinanceConnectionFailed
 from controllers.TradingInfo import TradeInfo
+from controllers.SetupGrids import SetupGrids
 from controllers.SchTrade import SchTrade
+from objects.GridPairs import GridPairs
 from views.Order import Order
 import traceback
 import sched
 from time import time, sleep
+import yaml
 
 __author__ = "Richard Chamberlain"
 __copyright__ = "Copyright 2022"
@@ -19,39 +22,35 @@ __status__ = "Dev"
 if __name__ == "__main__":        
     bc=BaseClass('config.ini')
     bc.log.info("Start of app")
-    qty_amount=0.62
-    step_amount=0.30
     
     try:
         
         ## Try to connect to the Binace Server
         connection=BinanceConnection(bc)
-        
-        ## Get Account Updates
-        trade_info=TradeInfo(bc,connection,'SOL','USDT',step_amount,qty_amount)
-    
-        trades_starter=trade_info.setup_new_trades()       
-    except BinanceConnectionFailed as e:
-        print("Applicaton fail to start.  Review the log file.")
-        
-    except:
-        bc.log.error("\t"+":"+traceback.format_exc())
-        print("Applicaton fail to start.  Review the log file.")
-    
-    ## Start the trading
-    try:
-        trading=SchTrade(bc,connection,\
-        trades_starter['war_chess'],\
-        trades_starter['price'],\
-        trades_starter['additional_orders'],step_amount,qty_amount)
-        loop_counter=0
-        trading.check_for_trade()
-        while True:
-            sleep(60 - time() % 60)
-	        # thing to run
-            trading.check_for_trade()
-            loop_count=loop_counter+1
-            if loop_count >10000:break
+        with open('yaml/starting.yaml', 'r') as file:
+            trades = yaml.safe_load(file) 
+  
+        pairs=[]
+        for trade in trades['Trades']:
+            print(trade)
+            pairs.append(GridPairs(bc,\
+                connection,\
+                trade['Token'],trade['BaseCur'],\
+                trade['InvestAmount'],\
+                trade['UpperLimit'],trade['LowerLimit'],\
+                trade['PriceStep'],trade['BayAmount']))
+            
+        x=1
+        while x<100:
+            for trade_grid in pairs:
+                trade_grid.create_order()
+            x=x+1
+        # while True:
+        #     sleep(60 - time() % 60)
+	    #     # thing to run
+        #     trading.check_for_trade()
+        #     loop_count=loop_counter+1
+        #     if loop_count >10000:break
     except:
         bc.log.error("\t"+":"+traceback.format_exc())
         print("Applicaton fail to start.  Review the log file.")
