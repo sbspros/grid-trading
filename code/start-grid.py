bc2e5d60695da92796df79076c2a758c9c50ff18
Richard Chamberlain <59230129+sbspros@users.noreply.github.com>
2022-11-09 06:46:19 -0600

    Update start-grid.py

--- code/start-grid.py
+++ code/start-grid.py
@@ -1,12 +1,7 @@
 from common.BaseClass import BaseClass,AppException
 from models.BinanceConnection import BinanceConnection,BinanceConnectionFailed
-from controllers.TradingInfo import TradeInfo
-from controllers.SetupGrids import SetupGrids
-from controllers.SchTrade import SchTrade
 from objects.GridPairs import GridPairs
-from views.Order import Order
 import traceback
-import sched
 from time import time, sleep
 import yaml
 
@@ -18,13 +13,10 @@ __maintainer__ = "Richard Chamberlain"
 __email__ = "richard@sbspros.ca"
 __status__ = "Dev"
 
-
 if __name__ == "__main__":        
     bc=BaseClass('config.ini')
     bc.log.info("Start of app")
-    
     try:
-        
         ## Try to connect to the Binace Server
         connection=BinanceConnection(bc)
         with open('yaml/starting.yaml', 'r') as file:
@@ -40,23 +32,10 @@ if __name__ == "__main__":
                 trade['UpperLimit'],trade['LowerLimit'],\
                 trade['PriceStep'],trade['BayAmount']))
 	    pairs[-1].get_open_orders
-	     
-            
-        x=1
-        while x<100:
-            for trade_grid in pairs:
-                trade_grid.create_order()
-            x=x+1
-
-	
-	
-        while True:
-	
-        #     sleep(60 - time() % 60)
-	    #     # thing to run
-        #     trading.check_for_trade()
-        #     loop_count=loop_counter+1
-        #     if loop_count >10000:break
+	while True:
+	    for grid_pair in pairs:
+		grid_pair.reconciliation()
+	    sleep(60 - time() % 60)
     except:
         bc.log.error("\t"+":"+traceback.format_exc())
         print("Applicaton fail to start.  Review the log file.")
